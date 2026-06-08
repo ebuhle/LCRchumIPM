61b33ee095a30a6dab570b3379777297638c8542
ebuhle <eric.buhle@mthoodenvironmental.com>
2026-06-08 11:33:07 -0700

    Try fitting model with unique `beta_alpha` for each H pop (#28)
    * Use factor `pop_type2` with levels `c("natural", "Duncan Hatchery", "Lewis Hatchery", "Grays Hatchery")` vs `pop_type` with levels `c("natural", "hatchery")`
    * Still leave hatchery `tau_M_obs` as `NA` and impute from hyperdistribution
    
    => `psi` estimates for `Duncan Hatchery` and `Lewis Hatchery` are pegged at 1
    => `psi` estimates for `Grays Hatchery` and `Grays MS` are more reasonable than in baseline model
    => However, the state estimates for all three hatcheries still show biased process errors
    => Fitted S-R functions for `Duncan Hatchery` and `Lewis Hatchery` still miss the observations, but S-R function for `Grays Hatchery` fits the data (not the states) reasonably well

--- analysis/R/03_LCRchumIPM_analysis.R
+++ analysis/R/03_LCRchumIPM_analysis.R
@@ -67,7 +67,7 @@ if(file.exists(here("analysis","results","LCRchumIPM.RData")))
 ## @knitr fit_Ricker
 fit_Ricker <- salmonIPM(stan_model = "IPM_LCRchum_pp", 
                         SR_fun = "Ricker", ages = list(M = 1), 
-                        par_models = list(psi ~ pop_type, s_MS ~ pop_type),
+                        par_models = list(psi ~ pop_type2, s_MS ~ pop_type),
                         center = FALSE, scale = FALSE, 
                         fish_data = fish_data, 
                         fecundity_data = fecundity_data,
