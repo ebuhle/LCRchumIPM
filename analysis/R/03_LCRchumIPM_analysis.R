681c434d2fcf030cc5e800325ce1aaafefdaf95f
ebuhle <eric.buhle@mthoodenvironmental.com>
2026-06-27 12:11:30 -0700

    Fit model with Beverton-Holt S-R function (#28)
    * As expected, psi (and hyperparameters) for natural populations is substantially higher and more uncertain under BH vs. Ricker
    * Other parameter and estimates don't change very much, and there is no glaring visual indication that either model "fits better"

--- analysis/R/03_LCRchumIPM_analysis.R
+++ analysis/R/03_LCRchumIPM_analysis.R
@@ -49,18 +49,29 @@ if(file.exists(here("analysis","results","LCRchumIPM.RData")))
 # ## @knitr print_fit_exp
 # print(fit_exp)
 # ## @knitr
-# 
-# # Beverton-Holt
-# ## @knitr fit_BH
-# fit_BH <- salmonIPM(stan_model = "IPM_LCRchum_pp", SR_fun = "BH", ages = list(M = 1), 
-#                     par_models = list(s_MS ~ pop_type), center = FALSE, scale = FALSE, 
-#                     fish_data = fish_data, fecundity_data = fecundity_data,
-#                     chains = 4, iter = 1500, warmup = 500,
-#                     control = list(max_treedepth = 15))
-# 
-# ## @knitr print_fit_BH
-# print(fit_BH)
-# ## @knitr
+
+# Beverton-Holt
+## @knitr fit_BH
+fit_BH <- salmonIPM(stan_model = "IPM_LCRchum_pp", 
+                    SR_fun = "BH", ages = list(M = 1), 
+                    par_models = list(psi ~ pop_type, s_MS ~ pop_type2),
+                    center = FALSE, scale = FALSE, 
+                    fish_data = fish_data, 
+                    fecundity_data = fecundity_data,
+                    chains = 4, iter = 1500, warmup = 500,
+                    control = list(max_treedepth = 15))
+
+## @knitr print_fit_BH
+print(fit_BH)
+## @knitr
+
+## @knitr prior_summary_BH
+prior_summary(fit_BH)
+## @knitr
+
+## @knitr plot_prior_posterior_BH
+plot_prior_posterior(fit_BH, pars = "p_D", include = FALSE)
+## @knitr
 
 # Ricker
 ## @knitr fit_Ricker
@@ -607,14 +618,14 @@ forecast_df <- fish_data_fore %>%
 metrics <- fit_Ricker %>% 
   as.matrix(c("S","M","mu_E","delta_NG","q","p_HOS","q_F","s_MS","b")) %>% as_draws_rvars() %>% 
   mutate_variables(#S_add_obs = fish_data$S_add_obs, p_local = 1 - S_add_obs/S, 
-                   p_G_obs = fish_data$p_G_obs, p_NG_obs = 1 - p_G_obs,
-                   E_hat = as.vector((q %**% mu_E) * q_F * (p_G_obs + delta_NG * p_NG_obs) * S),
-                   M0 = unsplit(sapply(split(M, fish_data$pop), lead), fish_data$pop),
-                   `M/E_hat` = M0/E_hat, q_3 = q[,1], q_4 = q[,2], q_5 = q[,3],
-                   R0 = unsplit(sapply(split(M*s_MS, fish_data$pop), lead), fish_data$pop),
-                   `R/S` = R0/S,
-                   b = replace(rep(rvar(0), nrow(fish_data)), 
-                               fish_data$B_take_obs > 0, b)) %>% 
+    p_G_obs = fish_data$p_G_obs, p_NG_obs = 1 - p_G_obs,
+    E_hat = as.vector((q %**% mu_E) * q_F * (p_G_obs + delta_NG * p_NG_obs) * S),
+    M0 = unsplit(sapply(split(M, fish_data$pop), lead), fish_data$pop),
+    `M/E_hat` = M0/E_hat, q_3 = q[,1], q_4 = q[,2], q_5 = q[,3],
+    R0 = unsplit(sapply(split(M*s_MS, fish_data$pop), lead), fish_data$pop),
+    `R/S` = R0/S,
+    b = replace(rep(rvar(0), nrow(fish_data)), 
+                fish_data$B_take_obs > 0, b)) %>% 
   subset_draws(variable = c("S","E_hat","M","M/E_hat","s_MS","R/S","b",
                             "q_3","q_4","q_5","p_HOS","q_F")) %>%
   summarize_draws(.med = median, "quantile2") %>% rename(`.05` = q5, `.95` = q95) %>%
@@ -622,9 +633,7 @@ metrics <- fit_Ricker %>%
   # filter(!forecast) %>%  select(-forecast) %>% 
   mutate(variable = gsub("\\[.*\\]", "", variable)) %>%
   pivot_longer(cols = c(.med, `.05`, `.95`), names_to = "summary", values_to = "value") %>%
-  mutate(value = replace(value, grepl("Hatchery", pop) & variable != "s_MS", NA),
-         # value = replace(value, !grepl("Duncan Channel", pop) & variable == "p_local", NA),
-         variable.summary = paste0(variable, summary)) %>% 
+  mutate(variable.summary = paste0(variable, summary)) %>% 
   select(-c(variable, summary)) %>%  
   pivot_wider(id_cols = c(pop, year), names_from = variable.summary, values_from = value) %>% 
   as.data.frame()
